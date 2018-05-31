ca68f9a2aba600d65c8c256e02f76d6eef0caf7e
ahmed-dinar <madinar.cse@gmail.com>
2018-06-01 03:55:26 +0600

    add drawer menu

--- app/src/main/java/com/tesseractinfosys/worldcup2018/WorldCupApp.java
+++ app/src/main/java/com/tesseractinfosys/worldcup2018/WorldCupApp.java
@@ -1,12 +1,17 @@
 package com.tesseractinfosys.worldcup2018;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 
 
 public class WorldCupApp extends AppCompatActivity {
 
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle aToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +19,24 @@ public class WorldCupApp extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        aToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(aToggle);
+        aToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
 
+        if(aToggle.onOptionsItemSelected(item)){
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 
 
